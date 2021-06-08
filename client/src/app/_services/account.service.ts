41294546962dac762b2f77d4ed3fa511c3c40f84
ronaldo.arriola <ronaldo.arriola@outlook.com>
2021-06-08 09:08:17 -0600

    Upload changes

---
+++ client/src/app/_services/account.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AccountService {
+
+  baseUrl: string = 'https://localhost:44385/api'
+
+  constructor(private http: HttpClient) { }
+
+  login(model: any)
+  {
+    return this.http.post(`${this.baseUrl}/account/login`, model)
+  }
+}
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,17 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from "@angular/common/http";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { FormsModule } from "@angular/forms";
 
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
